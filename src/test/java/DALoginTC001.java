66100ca11b9165622f7bc3376963ac54a1e16e05
dongthgtest <110093605+dongthgtest@users.noreply.github.com>
2024-09-24 22:41:51 +0700

    Update code 24 09 (#15)
    * Add selenium grid
    
    * Refactor code
    
    ---------
    
    Co-authored-by: Dong Tran <dongtran5295@gmail.com>

--- src/test/java/DALoginTC001.java
+++ src/test/java/DALoginTC001.java
@@ -1,18 +1,12 @@
 import com.agest.model.User;
 import com.agest.page.DashBoardPage;
 import com.agest.page.LoginPage;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 public class DALoginTC001 extends TestBase {
     private final User adminUser = User.getAdminUser();
     private final LoginPage loginPage = new LoginPage();
     private final DashBoardPage dashBoardPage = new DashBoardPage();
-
-    @AfterClass(alwaysRun = true)
-    public void afterClass() {
-        dashBoardPage.logOut();
-    }
 
     @Test(description = "Verify that user can login specific repository successfully via Dashboard login page with correct credentials")
     public void TC001() {
