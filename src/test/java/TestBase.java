9cee5fd159257d71b5de7b742e517110abd930c3
Dong Tran <dongtran5295@gmail.com>
2024-09-27 08:08:16 +0700

    update code

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.Configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,13 +12,13 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestBase {
     private final TestConfig testConfig = TestConfig.getInstance();
 
-    @Parameters({"remote"})
     @BeforeClass
-    public void setUp(@Optional("") String remote) {
-        log.info("------------------------------------------remote=" + System.getProperty("remote"));
+    public void setUp() {
+        log.info("Retry time : " + System.getProperty("maxRetryCount"));
         if (System.getProperty("remote").equals("true")) {
             Configuration.remote = testConfig.remote();
         }
+        log.info("Grid enable: " + System.getProperty("remote"));
         Configuration.browser = testConfig.getBrowser();
         Configuration.startMaximized = testConfig.isStartMaximized();
         Configuration.reportsFolder = testConfig.getReportFolder();
@@ -31,7 +29,6 @@ public class TestBase {
 
     @AfterClass
     public void tearDown() {
-        log.info("------------------------------------------REMOTE=" + System.getProperty("remote"));
         closeWebDriver();
     }
 }
