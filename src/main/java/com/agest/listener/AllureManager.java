e1adb44095760f161b6bd7c3775f18d47e1eef75
Dong Tran <dongtran5295@gmail.com>
2025-08-08 23:23:03 +0700

    fix comment

--- src/main/java/com/agest/listener/AllureManager.java
+++ src/main/java/com/agest/listener/AllureManager.java
@@ -2,7 +2,6 @@ package com.agest.listener;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
@@ -19,10 +18,5 @@ public class AllureManager {
 
     public static void setupAllureReporting() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    public static void saveLog(String name, String message) {
-        log.info("{}: {}", name, message);
-        Allure.addAttachment(name, "text/plain", message);
     }
 }
